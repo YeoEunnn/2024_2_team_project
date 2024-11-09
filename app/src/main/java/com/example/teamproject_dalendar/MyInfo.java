a2ae565dce6c955d73aeb3a94e2820340b4f1454
YeoEunnn <ayeoreunmy3456@naver.com>
2024-11-10 03:15:30 +0900

    초깃값

--- app/src/main/java/com/example/teamproject_dalendar/MyInfo.java
+++ app/src/main/java/com/example/teamproject_dalendar/MyInfo.java
@@ -1,16 +1,17 @@
 package com.example.teamproject_dalendar;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.TextView;
+// import com.google.firebase. ㅗㅗ
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -18,15 +19,22 @@ import com.google.firebase.database.DatabaseReference;
 public class MyInfo extends AppCompatActivity {
     private FirebaseAuth mFirebaseAuth;
     private DatabaseReference mDatabaseRef;
-    private EditText etEmail, etPwd;
-    private TextView tvIncorrectPwd;
+    private EditText edit_nickname, edit_introduction; // 닉네임, 한줄소개 수정
     private Button btnLogin;
-    private ImageButton imgBtnBackPress;
+    private ImageButton imgBtnBackPress; // 뒤로가기
+    private ImageView image_profile; // 프로필 사진
+    private Uri myImageUri;
+    //private UploadTask uploadTask; // 어떻게 해야댐;;;;
+    //private StorageReference storageReference; // ;;;;;;;;;;;;;;;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_info);
+    }
+
+    private void initImageViewProfile() {
+
     }
 
 
